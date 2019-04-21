2a8a25c861dd3548e4b5d42485c0f72d699812e0
Mateus Poi <mateuscppoi@gmail.com>
2019-04-21 20:24:19 -0300

    initial commit

---
+++ src/main/java/br/com/unicesumar/aep12/Aep12Application.java
@@ -0,0 +1,13 @@
+package br.com.unicesumar.aep12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aep12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aep12Application.class, args);
+	}
+
+}
